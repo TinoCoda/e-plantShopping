bef34684a1af0890ce99756ad9997bcb1fc43aeb
Destin B.Moanda <moanda@live.fr>
2026-02-08 10:22:25 -0500

    implement reducer functions

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,11 +7,34 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+        const {name,image,cost}=action.playload;
+        const existingItem=state.items.find(item => item.name===name)
+        if(existingItem){
+            // if the item already exists in the cart
+            existingItem.quatity++;
+        }else{
+            // add the item for the very first time
+            state.items.push({name,image,cost,quantity:1}); 
+        }
+
+  
     
     },
     removeItem: (state, action) => {
+        
+        const existingItem=state.items.find(item =>item.name===action.playload);
+        if(existingItem){
+            state.items=state.items.filter((item)=>item.name != action.playload);
+        }
     },
     updateQuantity: (state, action) => {
+        const {name,quantity}=action.playload,
+
+        const itemToUpdate=state.items.find(item =>item.name===name);
+        if(itemToUpdate){
+            itemToUpdate.quantity=quantity;
+        }
+
 
     
     },
